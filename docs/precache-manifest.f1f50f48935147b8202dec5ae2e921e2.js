127c962fd39e5d654c47750c447fcbb5b6e4fffa
Alejo Tapias <alejotapias0414@gmail.com>
2020-09-07 15:30:57 -0500

    docs created

---
+++ docs/precache-manifest.f1f50f48935147b8202dec5ae2e921e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4301153b78fc63ee08b8b2394b927419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae08eeefacaad65224fe",
+    "url": "/static/js/2.31608b09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31608b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f205877c8d537cf81c8",
+    "url": "/static/js/main.9b1f344b.chunk.js"
+  },
+  {
+    "revision": "4275e298ca4247238cbd",
+    "url": "/static/js/runtime-main.a18a15fd.js"
+  }
+]);
